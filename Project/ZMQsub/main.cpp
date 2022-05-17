4c833af564679505121a6a849704e82e4cf3bf8d
MartijnGuilliamsPXL <56915229+MartijnGuilliamsPXL@users.noreply.github.com>
2022-05-17 09:10:20 +0200

    update README

--- Project/ZMQsub/main.cpp
+++ Project/ZMQsub/main.cpp
@@ -6,33 +6,33 @@ int main( void )
 {
 
     try
-	{
-		zmq::context_t context(1);
+    {
+        zmq::context_t context(1);
 
-		//Incoming messages come in here
+        //Incoming messages come in here
         zmq::socket_t subscriber( context, ZMQ_SUB );
         //subscriber.connect( "tcp://localhost:24042" );
         subscriber.connect( "tcp://benternet.pxl-ea-ict.be:24042" );
         subscriber.setsockopt( ZMQ_SUBSCRIBE, "Zeeslag>", 8 );
 
-		zmq::message_t * msg = new zmq::message_t();
-		while( subscriber.connected() )
-		{
+        zmq::message_t * msg = new zmq::message_t();
+        while( subscriber.connected() )
+        {
 
-			subscriber.recv( msg );
+            subscriber.recv( msg );
             std::cout << std::string( (char*) msg->data()) << std::endl;
             if((msg->size()) == 3){
                 std::cout << "size is 3" << std::endl;
             }
-			std::cout << "Subscribed : [" << std::string( (char*) msg->data(), msg->size() ) << "]" << std::endl;
+            std::cout << "Subscribed : [" << std::string( (char*) msg->data(), msg->size() ) << "]" << std::endl;
 
             //subscriber.send( "test>", 5 );
         }
-	}
-	catch( zmq::error_t & ex )
-	{
-		std::cerr << "Caught an exception : " << ex.what();
+    }
+    catch( zmq::error_t & ex )
+    {
+        std::cerr << "Caught an exception : " << ex.what();
     }
 
-	return 0;
+    return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Network_Programming_2022
 
 - Zeeslag
+
+
+## Stappenplan
+* 1 voor 1 zenden en ontvangen (1 persoon)
+* 2 personen verbinden
+* om de beurt zenden en ontvangen voor beide spelers
+* server start een spel
+
+
+
+## Een spel is
+
+## Deel 1
+
+* elke speler heeft een speelveld (2d array)
+* een speelveld is vast maar wel verschillend
+* een speler kan een coördinaat kiezen om op te schieten (enkel raak/mis feedback)
+
+## Deel 2
+
+* 
+* 
+
+
+
+
+## Bronnen
+
+* https://zguide.zeromq.org/
